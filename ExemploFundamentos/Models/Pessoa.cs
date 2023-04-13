04968284547c7f35b2db2642b36ecc0d5e477e79
Vitor Goes <vitornevesgoes@gmail.com>
2023-04-13 16:52:39 -0300

    att modelo de classe

--- ExemploFundamentos/Models/Pessoa.cs
+++ ExemploFundamentos/Models/Pessoa.cs
@@ -7,6 +7,13 @@ namespace ExemploFundamentos.Models
 {
     public class Pessoa
     {
-        
+        public string? Nome { get; set; }
+        public int Idade { get; set; }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos");
+        }
+
     }
 }
