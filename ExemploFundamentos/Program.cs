c11f1a5609a0e2f1e176dd5de2aa2906919b86fb
Vitor Goes <vitornevesgoes@gmail.com>
2023-04-14 15:40:29 -0300

    add conversao de variaveis

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -1,13 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ExemploFundamentos.Models;
 
-
-DateTime dataAtual = DateTime.Now;
-Console.WriteLine(dataAtual.ToString("dd/MM/yyyy")); 
-
-
-
 // Pessoa p = new Pessoa();
 // p.Nome = "Vitor";
 // p.Idade = 21;
 // p.Apresentar();
+
+
+// DATETIME
+// DateTime dataAtual = DateTime.Now;
+// Console.WriteLine(dataAtual.ToString("dd/MM/yyyy")); 
+
+
+// CONVERTENDO VARIÁVEIS
+// int a = Convert.ToInt32("5");  // Convert é usado para converter outros tipos de objeto
+int a = int.Parse("5"); // Parse é um método usado somente para converter STRING em INTEIRO disparando exceptions, tem mais performance
+Console.WriteLine(a);
