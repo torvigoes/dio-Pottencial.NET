9e9da90a4139c87b51e31398a15f4f049e8fb1d3
Vitor Goes <vitornevesgoes@gmail.com>
2023-04-14 15:46:28 -0300

    conversao de strings

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -14,5 +14,9 @@ using ExemploFundamentos.Models;
 
 // CONVERTENDO VARIÁVEIS
 // int a = Convert.ToInt32("5");  // Convert é usado para converter outros tipos de objeto
-int a = int.Parse("5"); // Parse é um método usado somente para converter STRING em INTEIRO disparando exceptions, tem mais performance
+// int a = int.Parse("5"); // Parse é um método usado somente para converter STRING em INTEIRO disparando exceptions, tem mais performance
+// Console.WriteLine(a);
+
+int inteiro = 5;
+string a = inteiro.ToString();
 Console.WriteLine(a);
