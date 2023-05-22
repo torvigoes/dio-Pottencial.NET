6a923fed0552e4010c589b098ce130f703008596
Vitor Goes <vitornevesgoes@gmail.com>
2023-05-22 17:04:27 -0300

    add method to class Curso

--- ExemploExplorando/Models/Curso.cs
+++ ExemploExplorando/Models/Curso.cs
@@ -7,6 +7,12 @@ namespace ExemploExplorando.Models
 {
     public class Curso
     {
-        
+        public string Nome { get; set; }
+        public List<Pessoa> Alunos { get; set; }
+
+        public void AdicionarAluno(Pessoa aluno)
+        {
+            Alunos.Add(aluno);
+        }
     }
 }
