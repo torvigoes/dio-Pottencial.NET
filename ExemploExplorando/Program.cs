f42cd406816ff68e233fd3d2d628c43dffd1c81a
Vitor Goes <vitornevesgoes@gmail.com>
2023-05-29 15:46:08 -0300

    using constructors

--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -1,12 +1,7 @@
 ﻿using ExemploExplorando.Models;
 
-Pessoa p1 = new Pessoa();
-p1.Nome = "Vitor";
-p1.Sobrenome = "Góes";
-
-Pessoa p2 = new Pessoa();
-p2.Nome = "Julia";
-p2.Sobrenome = "Rodrigues";
+Pessoa p1 = new Pessoa("Vitor", "Góes");
+Pessoa p2 = new Pessoa("Julia", "Rodrigues");
 
 Curso cursoPt = new Curso();
 cursoPt.Nome = "Curso de Português";
