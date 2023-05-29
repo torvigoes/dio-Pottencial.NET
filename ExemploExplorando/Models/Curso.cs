dcdd5556e82785a0b2352eb9302877a61577a723
Vitor Goes <vitornevesgoes@gmail.com>
2023-05-29 15:17:08 -0300

    add new method RemoverAluno

--- ExemploExplorando/Models/Curso.cs
+++ ExemploExplorando/Models/Curso.cs
@@ -10,9 +10,15 @@ namespace ExemploExplorando.Models
         public string Nome { get; set; }
         public List<Pessoa> Alunos { get; set; }
 
+
         public void AdicionarAluno(Pessoa aluno)
         {
             Alunos.Add(aluno);
+        }
+
+        public bool RemoverAluno(Pessoa aluno)
+        {
+            return Alunos.Remove(aluno);
         }
 
         public int ObterQuantiaAlunosMatriculados()
